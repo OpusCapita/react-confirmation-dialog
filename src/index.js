30a08f306b8c55a0567b5f847d4e1c9047ae729d
Jenni Ristonmaa <Jenni.Ristonmaa@opuscapita.com>
2017-11-22 15:54:17 +0200

    Moved from oc-common-ui

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '../styles/main.scss';
+
+// export * from '@opuscapita/react-alerts';
+// export * from '@opuscapita/react-cards';
+// export * from '@opuscapita/react-icons';
+
+export * from './confirm-dialog/index.js';
