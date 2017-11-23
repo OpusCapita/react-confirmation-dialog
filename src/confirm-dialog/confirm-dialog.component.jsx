311f431871a21075930a635be72956f9083376d3
Jenni Ristonmaa <Jenni.Ristonmaa@opuscapita.com>
2017-11-23 10:48:39 +0200

    Requested changes

--- src/confirm-dialog/confirm-dialog.component.jsx
+++ src/confirm-dialog/confirm-dialog.component.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable react/no-string-refs */
-import React, { PropTypes } from 'react';
+import React from 'react';
+import { PropTypes } from 'prop-types';
 import { Modal, Button } from 'react-bootstrap';
-
-require('./confirm-dialog.scss');
 
 class ConfirmDialog extends React.PureComponent {
   render() {
